883abb4258d0c09422601244adb8f93f69fcc132
Stephen Sylvia <ssylvia@esri.com>
2013-09-06 12:03:46 -0400

    Allow for dev version of api

--- config.rb
+++ config.rb
@@ -19,12 +19,12 @@ set :css_dir, 'app/stylesheets'
 set :js_dir, 'app/javascript'
 set :images_dir, 'resources/images'
 set :fonts_dir, 'resources/fonts'
-set :isBuilding, false
+set :isProduction, false
 
 
 configure :build do
 
-set :isBuilding, true
+set :isProduction, true
 
 activate :minify_css
 activate :run_grunt
